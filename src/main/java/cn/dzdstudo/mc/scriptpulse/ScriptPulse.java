3c6c2a46f8194c38b38c5d47ef2e79fb3b0b6d70
HaiPaya <haipaya@dzdgame.cn>
2024-06-16 13:11:54 +0800

    初始提交

---
+++ src/main/java/cn/dzdstudo/mc/scriptpulse/ScriptPulse.java
@@ -0,0 +1,14 @@
+package cn.dzdstudo.mc.scriptpulse;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ScriptPulse extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
