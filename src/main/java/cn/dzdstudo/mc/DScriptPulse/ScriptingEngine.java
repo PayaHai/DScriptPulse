487fccba6c50532ed99c6fddfb6faecd2ccb6b55
HaiPaya <haipaya@dzdgame.cn>
2024-06-23 16:03:40 +0800

    优化细节 细化报错信息

--- src/main/java/cn/dzdstudo/mc/DScriptPulse/ScriptingEngine.java
+++ src/main/java/cn/dzdstudo/mc/DScriptPulse/ScriptingEngine.java
@@ -1,6 +1,8 @@
 package cn.dzdstudo.mc.DScriptPulse;
 
+import cn.dzdstudo.mc.DScriptPulse.ScriptingPluginAPI.Command;
 import cn.dzdstudo.mc.DScriptPulse.ScriptingPluginAPI.Listen;
+import cn.dzdstudo.mc.DScriptPulse.ScriptingPluginAPI.Minecraft;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
@@ -9,8 +11,10 @@ import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.HostAccess;
 import org.graalvm.polyglot.Source;
 
+import javax.script.ScriptException;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -30,11 +34,12 @@ public class ScriptingEngine extends JavaPlugin {
         logger.info("正在加载脚本插件" + pluginName + "。");
 
         Context context = Context.newBuilder("js")
-                .allowExperimentalOptions(true)
-                .allowHostAccess(HostAccess.ALL)
-                .allowHostClassLookup(className -> true)
+                //.allowExperimentalOptions(true)
+                //.allowHostAccess(HostAccess.ALL)
+                //.allowHostClassLookup(className -> true)
                 .option("js.commonjs-require", "true")
                 .option("js.commonjs-require-cwd", scriptPath.toString())
+                .option("engine.WarnInterpreterOnly", "false")
                 .allowAllAccess(true)
                 .build();
 
@@ -42,20 +47,24 @@ public class ScriptingEngine extends JavaPlugin {
         // 日志
         context.getBindings("js").putMember("logger", new cn.dzdstudo.mc.DScriptPulse.ScriptingPluginAPI.Logger(logger, pluginName));
         // Minecraft
-        context.getBindings("js").putMember("mc", new cn.dzdstudo.mc.DScriptPulse.ScriptingPluginAPI.Minecraft());
+        context.getBindings("js").putMember("minecraft", new Minecraft());
         // 监听事件
         context.getBindings("js").putMember("listen", new Listen());
         // 命令系统
-        context.getBindings("js").putMember("command", new cn.dzdstudo.mc.DScriptPulse.ScriptingPluginAPI.Command(plugin));
+        context.getBindings("js").putMember("command", new Command(plugin));
 
         // 执行主 JavaScript 文件
         try {
             // 读取脚本
             Source source = Source.newBuilder("js", new File(scriptPath.toFile() + "/" + pluginMain)).build();
             context.eval(source);
-        }
-        catch (Exception e) {
-            logger.severe("脚本插件" + pluginName + "加载出错:" + e);
+        } catch (Exception e) {
+            logger.severe("脚本插件" + pluginName + "加载出错:");
+            logger.severe("异常信息: " + e.getMessage());
+            StackTraceElement[] stackTraceElements = e.getStackTrace();
+            StackTraceElement firstElement = stackTraceElements[0];
+            logger.severe("异常发生在: " + firstElement.getClassName() + "." + firstElement.getMethodName()
+                    + "() 位于 " + firstElement.getLineNumber() + " 行。");
         }
 
         logger.info("脚本插件" + pluginName + "加载完成。");
