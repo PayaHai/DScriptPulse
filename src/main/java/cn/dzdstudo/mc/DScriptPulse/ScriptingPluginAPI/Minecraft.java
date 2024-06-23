43f27b8dcb582f2e4d707a77a8873c54b91e0817
HaiPaya <haipaya@dzdgame.cn>
2024-06-23 16:04:18 +0800

    增加 API 修复脚本插件无法调用的 BUG

--- src/main/java/cn/dzdstudo/mc/DScriptPulse/ScriptingPluginAPI/Minecraft.java
+++ src/main/java/cn/dzdstudo/mc/DScriptPulse/ScriptingPluginAPI/Minecraft.java
@@ -1,7 +1,9 @@
 package cn.dzdstudo.mc.DScriptPulse.ScriptingPluginAPI;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.World;
+import org.graalvm.polyglot.HostAccess;
 
 import java.util.List;
 
@@ -10,7 +12,8 @@ public class Minecraft {
      * 获取玩家对象
      * @param player 玩家名称/Uuid
      */
-    public static cn.dzdstudo.mc.DScriptPulse.ScriptingPluginAPI.Player getPlayer(String player) {
+    @HostAccess.Export
+    public cn.dzdstudo.mc.DScriptPulse.ScriptingPluginAPI.Player getPlayer(String player) {
         return new cn.dzdstudo.mc.DScriptPulse.ScriptingPluginAPI.Player(org.bukkit.Bukkit.getPlayer(player));
     }
 
@@ -19,7 +22,7 @@ public class Minecraft {
      *
      * @return 世界列表
      */
-    public static List<cn.dzdstudo.mc.DScriptPulse.ScriptingPluginAPI.World> getWeather() {
+    public List<cn.dzdstudo.mc.DScriptPulse.ScriptingPluginAPI.World> getWeather() {
         List<World> worlds = Bukkit.getServer().getWorlds();
         List<cn.dzdstudo.mc.DScriptPulse.ScriptingPluginAPI.World> worldList = new java.util.ArrayList<>();
         for(World world : worlds) {
@@ -27,5 +30,29 @@ public class Minecraft {
         }
 
         return worldList;
+    }
+
+    /**
+     * 获取世界对象
+     *
+     * @param worldName 世界名称
+     * @return 世界对象
+     */
+    public cn.dzdstudo.mc.DScriptPulse.ScriptingPluginAPI.World getWorld(String worldName) {
+        return new cn.dzdstudo.mc.DScriptPulse.ScriptingPluginAPI.World(worldName);
+    }
+
+    /**
+     * 获取坐标对象
+     *
+     * @param world 世界对象
+     * @param x X坐标
+     * @param y Y坐标
+     * @param z Z坐标
+     *
+     * @return 坐标对象
+     */
+    public cn.dzdstudo.mc.DScriptPulse.ScriptingPluginAPI.Pos getPos(cn.dzdstudo.mc.DScriptPulse.ScriptingPluginAPI.World world, double x, double y, double z) {
+        return new cn.dzdstudo.mc.DScriptPulse.ScriptingPluginAPI.Pos(new Location(Bukkit.getWorld(world.getName()), x, y, z));
     }
 }
